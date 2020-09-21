0f40a4018604016f4ac170dbd91a363cc4793d3b
Chambers Mansour <chambersmansour97@gmail.com>
2020-09-21 15:13:02 -0500

    added constructor in employee.js

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,11 @@
 // TODO: Write code to define and export the Employee class
+function Employee (name, id, ) {
+    this.name
+    this.id
+    this.email
+    this.getName()
+    this.getId()
+    this.getEmail()
+    // get role return 'Employee' not sure what that does yet
+    this.getRole()
+}
