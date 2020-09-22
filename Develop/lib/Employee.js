323e1cbcd77d56473ed183cc51acad493f777cdf
Chambers Mansour <chambersmansour97@gmail.com>
2020-09-21 20:24:12 -0500

    added module.exports to js files

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,11 +1,18 @@
 // TODO: Write code to define and export the Employee class
-function Employee (name, id, ) {
-    this.name
-    this.id
-    this.email
-    this.getName()
+function Employee (name, id, email ) {
+    this.name = name
+    this.id = id
+    this.email = email
     this.getId()
     this.getEmail()
     // get role return 'Employee' not sure what that does yet
     this.getRole()
 }
+
+new Employee ();
+
+//it's like import or exchange or something
+//it takes the object the constructor function makes and takes it off the page
+//to one of the designated ones: Engineer, Intern, and Manager
+// export.(???)
+module.exports = Employee;
